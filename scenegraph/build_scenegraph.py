eeda13bb1f688b993801330cb24e0190269107c3
Ubuntu <ubuntu@c1.jemonra.mapir>
2024-05-02 19:58:13 +0200

    first commit

---
+++ scenegraph/build_scenegraph.py
@@ -0,0 +1,13 @@
+
+import argparse
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="TODO: progra description")
+
+    parser.add_argument("-m", 
+                        "--mode", 
+                        help="Script mode", 
+                        choices=)
+
+    main()
